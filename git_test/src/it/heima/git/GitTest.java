688c1ed801ed23f583b8390f1853f34fb60b48bb
wyy_sax <sa2sac@163.com>
2019-04-03 16:09:54 +0800

    add git-test java project in git repository with IDE

---
+++ git_test/src/it/heima/git/GitTest.java
@@ -0,0 +1,9 @@
+package it.heima.git;
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("GIT-TEST~~~~");
+        System.out.println("GIT-TEST~~~~");
+        System.out.println("GIT-TEST~~~~");
+    }
+}
